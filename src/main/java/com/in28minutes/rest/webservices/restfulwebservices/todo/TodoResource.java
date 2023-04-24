b63e8a0da32b5f1a3ae64f14781e36288773070d
chiho <ramtam611@gmail.com>
2023-04-23 22:26:18 -0700

    259. Step 05 - Full Stack React & Spring Boot with JPA & Hibernate - Connect MySql

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import com.in28minutes.rest.webservices.restfulwebservices.todo.repository.TodoRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,42 +8,44 @@ import java.util.List;
 
 @RestController
 public class TodoResource {
-    private TodoService todoService;
+    private TodoRepository todoRepository;
 
-    public TodoResource(TodoService todoService) {
-        this.todoService = todoService;
+    public TodoResource(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
     @GetMapping("/todos")
     public List<Todo> retrieveAllTodos() {
-        return todoService.findAll();
+        return todoRepository.findAll();
     }
 
     @GetMapping("/user/{username}/todos")
     public List<Todo> retrieveTodosByUser(@PathVariable String username) {
-        return todoService.findByUsername(username);
+        return todoRepository.findByUsername(username);
     }
 
     @GetMapping("/todo/{id}")
     public Todo retrieveTodoByID(@PathVariable int id) {
-        return todoService.findById(id);
+        return todoRepository.findById(id).get();
     }
 
-    @PostMapping("/user/{username}/todo")
-    public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
-        Todo createdTodo = todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),todo.isDone());
+    @PostMapping("/todo")
+    public Todo createTodo(@RequestBody Todo todo) {
+//        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        todo.setId(null);
+        Todo createdTodo = todoRepository.save(todo);
         return createdTodo;
     }
 
     @DeleteMapping("/todo/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable int id) {
-        todoService.deleteById(id);
+        todoRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/todo/{id}")
     public Todo updateTodo(@RequestBody Todo todo) {
-        todoService.updateTodo(todo);
+        todoRepository.save(todo);
         return todo;
     }
 }
